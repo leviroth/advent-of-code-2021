4311249d95cb8feb4d2d2246522240fdb752fa6e
Levi Roth <levimroth@gmail.com>
2021-12-11 12:48:02 -0500

    Day 6, part 1

--- src/day06.ml
+++ src/day06.ml
@@ -10,12 +10,8 @@ end)
 module Common = struct
   module Input = Input
   module Output = Int
-end
 
-module Part_01 = struct
-  include Common
-
-  let solve input =
+  let solve days input =
     let counters =
       List.fold
         input
@@ -25,7 +21,7 @@ module Part_01 = struct
               | None -> 1
               | Some n -> n + 1))
     in
-    List.range 0 80
+    List.range 0 days
     |> List.fold ~init:counters ~f:(fun counters _ ->
            Map.to_alist counters
            |> List.concat_map ~f:(fun (days, num_fish) ->
@@ -36,11 +32,29 @@ module Part_01 = struct
     |> Map.data
     |> List.sum (module Int) ~f:Fn.id
   ;;
+end
+
+module Part_01 = struct
+  include Common
+
+  let solve = solve 80
 
   let%expect_test _ =
     let test_case = Input.of_string "3,4,3,1,2" in
     print_s [%sexp (solve test_case : int)];
     [%expect {| 5934 |}]
+  ;;
+end
+
+module Part_02 = struct
+  include Common
+
+  let solve = solve 256
+
+  let%expect_test _ =
+    let test_case = Input.of_string "3,4,3,1,2" in
+    print_s [%sexp (solve test_case : int)];
+    [%expect {| 26984457539 |}]
   ;;
 end
 
