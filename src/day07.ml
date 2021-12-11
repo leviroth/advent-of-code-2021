342375f5ed910815eb0b5c6c8c2890420917df3e
Levi Roth <levimroth@gmail.com>
2021-12-11 12:54:12 -0500

    Day 7, part 2

--- src/day07.ml
+++ src/day07.ml
@@ -10,21 +10,28 @@ end)
 module Common = struct
   module Input = Input
   module Output = Int
-end
 
-module Part_01 = struct
-  include Common
-
-  let solve input =
+  let solve input ~fuel_cost =
     let min = List.reduce_exn input ~f:min in
     let max = List.reduce_exn input ~f:max in
     List.map
       (List.range ~stop:`inclusive min max)
       ~f:(fun position ->
-        List.sum (module Int) input ~f:(fun crab -> abs (crab - position)))
+        List.sum
+          (module Int)
+          input
+          ~f:(fun crab ->
+            let distance = abs (position - crab) in
+            fuel_cost distance))
     |> List.min_elt ~compare
     |> Option.value_exn
   ;;
+end
+
+module Part_01 = struct
+  include Common
+
+  let solve = solve ~fuel_cost:Fn.id
 
   let%expect_test _ =
     let test_case = Input.of_string "16,1,2,0,4,2,7,1,2,14" in
@@ -33,4 +40,23 @@ module Part_01 = struct
   ;;
 end
 
-let parts : (module Solution.Part) list = [ (module Part_01) ]
+module Part_02 = struct
+  include Common
+
+  let solve =
+    let triangle_number n =
+      match n with
+      | 0 -> 0
+      | _ -> n * (n + 1) / 2
+    in
+    solve ~fuel_cost:triangle_number
+  ;;
+
+  let%expect_test _ =
+    let test_case = Input.of_string "16,1,2,0,4,2,7,1,2,14" in
+    print_s [%sexp (solve test_case : int)];
+    [%expect {| 168 |}]
+  ;;
+end
+
+let parts : (module Solution.Part) list = [ (module Part_01); (module Part_02) ]
