c0fae315442d18b02cd2a0c779dc940ddb35e4b1
Levi Roth <levimroth@gmail.com>
2021-12-12 12:38:16 -0500

    Day 9, part 2

--- src/day09.ml
+++ src/day09.ml
@@ -18,28 +18,31 @@ end
 module Common = struct
   module Input = Input.Make_parseable (Grid)
   module Output = Int
+
+  let get t (row, col) =
+    match t.(row).(col) with
+    | v -> Some v
+    | exception Invalid_argument _ -> None
+  ;;
+
+  let find_low_points t =
+    let neighbors point = Int_pair.neighbors point Int_pair.right_vectors in
+    let rows = List.range 0 (Array.length t) in
+    let columns = List.range 0 (Array.length t.(0)) in
+    List.filter (List.cartesian_product rows columns) ~f:(fun coords ->
+        let point = Option.value_exn (get t coords) in
+        let neighbors = neighbors coords in
+        let neighbors = List.filter_map neighbors ~f:(get t) in
+        List.for_all neighbors ~f:(fun neighbor -> point < neighbor))
+  ;;
 end
 
 module Part_01 = struct
   include Common
 
   let solve t =
-    let get (row, col) =
-      match t.(row).(col) with
-      | v -> Some v
-      | exception Invalid_argument _ -> None
-    in
-    let neighbors point = Int_pair.neighbors point Int_pair.right_vectors in
-    let rows = List.range 0 (Array.length t) in
-    let columns = List.range 0 (Array.length t.(0)) in
-    List.filter_map (List.cartesian_product rows columns) ~f:(fun coords ->
-        let point = Option.value_exn (get coords) in
-        let neighbors = neighbors coords in
-        let neighbors = List.filter_map neighbors ~f:get in
-        match List.for_all neighbors ~f:(fun neighbor -> point < neighbor) with
-        | true -> Some point
-        | false -> None)
-    |> List.map ~f:succ
+    find_low_points t
+    |> List.map ~f:(fun coords -> Option.value_exn (get t coords) + 1)
     |> List.sum (module Int) ~f:Fn.id
   ;;
 
@@ -56,4 +59,51 @@ module Part_01 = struct
   ;;
 end
 
-let parts : (module Solution.Part) list = [ (module Part_01) ]
+module Part_02 = struct
+  include Common
+
+  let count_basin t coords =
+    let visited = Int_pair.Hash_set.create () in
+    let queue = Queue.create () in
+    let enqueue_if_not_visited coords =
+      match Hash_set.mem visited coords with
+      | true -> ()
+      | false ->
+        Hash_set.add visited coords;
+        Queue.enqueue queue coords
+    in
+    enqueue_if_not_visited coords;
+    while not (Queue.is_empty queue) do
+      let current = Queue.dequeue_exn queue in
+      List.iter (Int_pair.neighbors current Int_pair.right_vectors) ~f:(fun coords ->
+          match get t coords with
+          | None -> ()
+          | Some v ->
+            (match v with
+            | 9 -> ()
+            | _ -> enqueue_if_not_visited coords))
+    done;
+    Hash_set.length visited
+  ;;
+
+  let solve t =
+    find_low_points t
+    |> List.map ~f:(count_basin t)
+    |> List.sort ~compare:(Comparable.reverse compare)
+    |> fun l -> List.take l 3 |> List.reduce_exn ~f:( * )
+  ;;
+
+  let%expect_test _ =
+    let test_case =
+      Input.of_string {|2199943210
+3987894921
+9856789892
+8767896789
+9899965678|}
+    in
+    print_s [%sexp (solve test_case : int)];
+    [%expect {| 1134 |}]
+  ;;
+end
+
+let parts : (module Solution.Part) list = [ (module Part_01); (module Part_02) ]
